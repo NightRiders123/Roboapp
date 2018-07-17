3b447d95589bfd4d15334decd10002345105c878
ajwillrock <abdul.jabbar.khalid@gmail.com>
2018-07-17 10:46:54 +0500

    Starting Redux

--- src/index.js
+++ src/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App.js'
+import App from './containers/App.js'
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons';
 
 ReactDOM.render(
 	<App />,
-	 document.getElementById('root'));
+	 document.getElementById('root')
+);
 
 registerServiceWorker();
